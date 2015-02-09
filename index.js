77a6ca22d4e291a650c3b1ced53ce91ff8c92647
Juha Komulainen <juha.komulainen@evident.fi>
2015-02-09 18:45:47 +0200

    More settings.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 "use strict";
 
 var settings = exports.settings = {
+    exitOnFailure: true,
+    staticBundle: false,
+
     serve: {
         port: 3000
+    },
+
+    paths: {
+        output: './build/gulp'
     }
 };
 
@@ -35,22 +42,18 @@ exports.register = function(gulp) {
     var watchify        = require('watchify');
 
     var config = {
-        // Build production version? 'true' will produce optimized assets, 'false' will leave debugging information.
-        // Note that generally you don't need to touch this but just call 'build-production' task.
-        production: false,
-
         // Should we start watching for changes?
         watch: true
     };
 
-// Paths for various assets.
+    // Paths for various assets.
     var paths = {
         sass: './src/css/*.scss',
         templates: './src/templates/**/*.html',
         views: './src/views/**/*.hbs',
         build: {
-            dest: './build/gulp/static',
-            tmp: './build/gulp/tmp'
+            dest: path.join(settings.paths.output, 'static'),
+            tmp: path.join(settings.paths.output, 'tmp')
         },
         vendor: {
             stylesheets: [
@@ -65,20 +68,26 @@ exports.register = function(gulp) {
         }
     };
 
-    var handleErrors = function() {
+    function handleErrors() {
+        /*jshint validthis:true */
         // Send error to notification center with gulp-notify
         notify.onError({
             title: "Build Error",
             message: "<%= error.message %>"
         }).apply(this, arguments);
 
-        // Keep gulp from hanging on this task
-        this.emit('end');
-    };
+        if (settings.exitOnFailure) {
+            process.exit(1);
+        }
 
-// Read 'package.json' to see which external libraries we are using and mark them
-// as external libraries for browserify. This lets us produce different bundles for
-// external libraries and our own code, making the incremental builds faster.
+        // Keep gulp from hanging on this task
+        //noinspection JSHint
+        this.emit('end');
+    }
+
+    // Read 'package.json' to see which external libraries we are using and mark them
+    // as external libraries for browserify. This lets us produce different bundles for
+    // external libraries and our own code, making the incremental builds faster.
     var externalLibraries = [];
     var browserDependencies = require('./package.json').browser;
     for (var key in browserDependencies) {
@@ -86,11 +95,11 @@ exports.register = function(gulp) {
             externalLibraries.push(key);
     }
 
-// Create a bundle containing all external libraries.
+    // Create a bundle containing all external libraries.
     gulp.task('compile-libs', function () {
 
         /* @type {Object} */
-        var bundler = browserify({debug: !config.production});
+        var bundler = browserify({debug: !settings.staticBundle});
         bundler.transform(browserifyShim);
 
         for (var key in browserDependencies)
@@ -102,22 +111,22 @@ exports.register = function(gulp) {
         return bundler.bundle()
             .on('error', handleErrors)
             .pipe(source('libs.js'))
-            .pipe(gulpif(config.production, streamify(uglify())))
+            .pipe(gulpif(settings.staticBundle, streamify(uglify())))
             .pipe(gulp.dest(path.join(paths.build.dest, 'js')));
     });
 
-// Create a bundle containing our own dependencies.
+    // Create a bundle containing our own dependencies.
     gulp.task('compile-js', ['compile-libs', 'compile-angular-templates'], function () {
 
         // Variables which will be inlined to src/js/config.js
         var env = {
-            API_BASE: config.production ? '/api' : 'http://localhost:8080/api',
-            USE_TEMPLATE_CACHE: config.production,
-            DEBUG_LOGGING: !config.production
+            API_BASE: settings.staticBundle ? '/api' : 'http://localhost:8080/api',
+            USE_TEMPLATE_CACHE: settings.staticBundle,
+            DEBUG_LOGGING: !settings.staticBundle
         };
 
         /* @type {Object} */
-        var bundler = browserify('./src/js/main.js', {debug: config.production, cache: {}, packageCache: {}, fullPaths: true});
+        var bundler = browserify('./src/js/main.js', {debug: settings.staticBundle, cache: {}, packageCache: {}, fullPaths: true});
         if (config.watch)
             bundler = watchify(bundler);
 
@@ -139,14 +148,14 @@ exports.register = function(gulp) {
             return bundler.bundle()
                 .on('error', handleErrors)
                 .pipe(source('app.js'))
-                .pipe(gulpif(config.production, streamify(uglify())))
+                .pipe(gulpif(settings.staticBundle, streamify(uglify())))
                 .pipe(gulp.dest(path.join(paths.build.dest, 'js')));
         }
 
         return rebundle();
     });
 
-// Starts an express server serving the static resources and begins watching changes
+    // Starts an express server serving the static resources and begins watching changes
     gulp.task('serve', ['watch'], function() {
 
         /** @type Object */
@@ -170,10 +179,10 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Compiles Sass stylesheets to CSS
+    // Compiles Sass stylesheets to CSS
     gulp.task('sass', function() {
         var options = { };
-        if (config.production) {
+        if (settings.staticBundle) {
             options.outputStyle = 'compressed';
         } else {
             options.outputStyle = 'nested';
@@ -191,14 +200,14 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Copies the stylesheets of EpicEditor to proper place
+    // Copies the stylesheets of EpicEditor to proper place
     gulp.task('epiceditor-css', function() {
         return gulp.src('./bower_components/epiceditor/epiceditor/**/*.css')
             .pipe(gulp.dest(path.join(paths.build.dest, 'css/epiceditor')))
             .on('error', handleErrors);
     });
 
-// Creates a bundle from vendor CSS files
+    // Creates a bundle from vendor CSS files
     gulp.task('vendor-css', ['epiceditor-css'], function() {
         return gulp.src(paths.vendor.stylesheets)
             .pipe(concatCss("vendor-bundle.css"))
@@ -207,24 +216,24 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Copies fonts to proper place
+    // Copies fonts to proper place
     gulp.task('fonts', function() {
         return gulp.src(paths.vendor.fonts)
             .pipe(gulp.dest(path.join(paths.build.dest, 'fonts')))
             .on('error', handleErrors);
     });
 
-// Builds all styles
+    // Builds all styles
     gulp.task('styles', ['sass', 'vendor-css', 'fonts']);
 
-// Starts watching for changes
+    // Starts watching for changes
     gulp.task('watch', ['build'], function() {
         //gulp.watch(['./src/js/**/*.js'], ['compile-js']);
         gulp.watch(paths.sass, ['sass']);
         gulp.watch(paths.views, ['compile-views']);
     });
 
-// Compiles handlebars templates to html
+    // Compiles handlebars templates to html
     gulp.task('compile-views', function() {
         return gulp.src(paths.views)
             .pipe(handlebars({}))
@@ -235,7 +244,7 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Compiles angular templates to a single JavaScript module which populates the template-cache
+    // Compiles angular templates to a single JavaScript module which populates the template-cache
     gulp.task('compile-angular-templates', function () {
         return gulp.src(paths.templates)
             .pipe(templateCache({
@@ -248,7 +257,7 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Runs JavaScript unit tests
+    // Runs JavaScript unit tests
     gulp.task('test', function() {
         return gulp.src("./test/unit/**/*_spec.js")
             .pipe(karma({
@@ -258,7 +267,7 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Runs JavaScript end-to-end tests. Requires that the application and webdriver-manager are running.
+    // Runs JavaScript end-to-end tests. Requires that the application and webdriver-manager are running.
     gulp.task('test-e2e', function() {
         return gulp.src(["./test/e2e/**/*_spec.js"])
             .pipe(protractor({
@@ -267,39 +276,39 @@ exports.register = function(gulp) {
             .on('error', handleErrors);
     });
 
-// Build all templates
+    // Build all templates
     gulp.task('templates', ['compile-views', 'compile-angular-templates']);
 
-// Cleans everything built
+    // Cleans everything built
     gulp.task('clean', function (cb) {
-        del('build/gulp', cb);
+        del(settings.paths.output, cb);
     });
 
-// Builds everything
+    // Builds everything
     gulp.task('build', ['compile-js', 'styles', 'templates']);
 
-// Create an optimized build
+    // Create an optimized build
     gulp.task('build-optimized', ['build'], function() {
         return gulp.src(path.join(paths.build.dest, '**'))
             .pipe(revall({ ignore: [/^index.html$/, /^css\/epiceditor\/.+/] }))
-            .pipe(gulp.dest('./build/gulp/optimized'));
+            .pipe(gulp.dest(path.join(settings.paths.output, 'optimized')));
     });
 
-// Creates a production build
+    // Creates a production build
     gulp.task('build-production', ['clean'], function() {
-        config.production = true;
+        settings.staticBundle = true;
         config.watch = false;
         gulp.start('build-optimized');
     });
 
-// Creates a development build
+    // Creates a development build
     gulp.task('build-development', ['clean'], function() {
-        config.production = false;
+        settings.staticBundle = false;
         config.watch = false;
         gulp.start('build');
     });
 
-// By default, clean everything built and start local server
+    // By default, clean everything built and start local server
     gulp.task('default', ['clean'], function () {
         gulp.start('serve');
     });

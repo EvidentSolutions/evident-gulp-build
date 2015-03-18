a80957a15d7fd86b4f7e2aa2cb2f4a0a4ac9f913
Juha Komulainen <juha.komulainen@evident.fi>
2015-03-18 15:07:19 +0200

    Added TODO-comments.

--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 
 exports.registerDefaultTasks = require('./lib/main').registerTasks;
 exports.settings = require('./lib/settings');
+
+// TODO: this is temporarily exposed, you should not need to import this
 exports.paths = require('./lib/paths');
+
+// TODO: this is temporarily exposed, you should not need to import this
 exports.errorHandler = require('./lib/error-handler');
